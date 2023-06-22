caa5595f23fdf72e25ee0d1f4ad2d2865a5fb49f
szaccagni <sarahzaccagni@gmail.com>
2023-06-22 18:28:48 -0400

    adds clerk auth and navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import { SignInButton, SignedOut, UserButton, useUser } from '@clerk/nextjs'
+
+export default function NavBar() {
+    const { isSignedIn } = useUser()
+
+    return (
+        <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
+            <div className='container mx-auto w-full flex justify-between items-center'>
+                <div>Task Tailor</div>
+                <SignedOut>
+                    <SignInButton mode="modal"></SignInButton>
+                </SignedOut>
+                { isSignedIn && 
+                    <UserButton 
+                    appearance={{
+                    elements: {
+                        userButtonAvatarBox: {
+                        width: 56,
+                        height: 56
+                        }
+                    }
+                    }}
+                    afterSignOutUrl='/'
+                />
+                }
+            </div>
+        </div>
+    )
+}
